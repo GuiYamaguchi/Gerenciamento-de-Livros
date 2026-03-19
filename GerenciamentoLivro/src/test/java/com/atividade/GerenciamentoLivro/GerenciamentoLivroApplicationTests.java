86864eaa1d1fe49090c42815fac89572e8d50051
unknown <japajao25@gmail.com>
2026-03-19 09:53:03 -0300

    entidade criada

---
+++ GerenciamentoLivro/src/test/java/com/atividade/GerenciamentoLivro/GerenciamentoLivroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atividade.GerenciamentoLivro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoLivroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
